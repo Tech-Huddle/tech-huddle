bc7858f41291c75af7bfbd802aeb6f7e950b51d3
Anupam Datta <anupam.datta@mettletech.in>
2023-01-02 13:30:27 +0530

    db connection done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const connection=require('./connection/db')
 require('dotenv').config()
 const PORT = process.env.PORT||5000;
 const app = express();
@@ -11,6 +12,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/',(req, res, next) =>{
 	res.send({"message":"hello world!"})
+	})
+
+app.get('/task/list',(req, res, next) =>{
+	connection.query("select * from task", function (error, results, fields) {
+		if (error) {
+			console.log(error)
+		}
+		else{
+			res.send({"data": results})
+		}
+	
+	  });
 	})
 
 app.listen(PORT, () => {
