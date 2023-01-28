e23f4a6c60110c1f89f291d43d30605dfef35ee7
Anupam Datta <anupam.datta@mettletech.in>
2023-01-28 21:31:13 +0530

    init sql updated

--- init.sql
+++ init.sql
@@ -8,3 +8,21 @@ CREATE TABLE IF NOT EXISTS `task` (
   `date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+CREATE TABLE IF NOT EXISTS `user_secret_info` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `temp_password` text DEFAULT NULL,
+  `attempt` int(10) DEFAULT NULL,
+  `status` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
